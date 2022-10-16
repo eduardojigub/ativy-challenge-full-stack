881483b645c8f8e23be0a367f679ba98b9a77b7d
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 12:03:48 -0300

    feat: connecting Atlas to the project

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
