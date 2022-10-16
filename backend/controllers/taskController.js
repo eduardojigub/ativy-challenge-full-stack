da9247de9d2f3dbe66feef9f715d3c4d92bf68f6
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 14:06:52 -0300

    feat: creating getuserCurrentTask to get only tasks for a single user

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -24,3 +24,12 @@ export const getAllTasks = async (req, res, next) => {
     next(err);
   }
 };
+
+export const getCurrentUserTasks = async (req, res, next) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id });
+    res.status(200).json(tasks);
+  } catch (err) {
+    next(err);
+  }
+};
--- backend/routes/tasksRouter.js
+++ backend/routes/tasksRouter.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import { createTask, getAllTasks } from '../controllers/taskController.js';
+import { createTask, getAllTasks, getCurrentUserTasks } from '../controllers/taskController.js';
 
 const router = express.Router();
 
 router.post('/', createTask);
 router.get('/all', getAllTasks);
+router.get('/myTasks', getCurrentUserTasks);
 
 export default router;
