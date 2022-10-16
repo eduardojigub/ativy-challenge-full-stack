34cbc13f3c7b83d9b2e82a0a4cc6d62828c242f8
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 17:36:32 -0300

    feat: styling login component and adding global scss

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import classes from './AuthForm.module.scss';
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className={classes.register}>
+      <h1 className={classes.title}>
+        Login
+      </h1>
+      <form className={classes.authForm}>
+        <label htmlFor="email">
+          Email
+          <input type="email" name="email" placeholder="Digite seu e-mail" required />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type="password" name="password" placeholder="Digite sua senha" required />
+        </label>
+      </form>
+    </div>
   );
 }
 
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Login from '../components/auth/Login';
 import Register from '../components/auth/Register';
+import classes from './Auth.module.scss';
 
 function Auth() {
   return (
     <Layout>
-      <div>
+      <div className={classes.form_container}>
         <Login />
         <Register />
       </div>
