3383b86873658fc99eadc99ec180885a8e5f733c
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 13:11:31 -0300

    Feat: adding checkAuth as middleware to user and tasks routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,12 @@ import express from 'express';
 import authRoutes from './authRouter.js';
 import usersRoutes from './usersRouter.js';
 import tasksRoutes from './tasksRouter.js';
+import checkAuth from '../middlewares/checkAuth.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
-router.use('/users', usersRoutes);
-router.use('/tasks', tasksRoutes);
+router.use('/users', checkAuth, usersRoutes);
+router.use('/tasks', checkAuth, tasksRoutes);
 
 export default router;
