4bcd26f21384526b35bf4833058b613e5af2d5ad
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 16:56:51 -0300

    feat: creating Toaster and initial Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import EditProfile from './pages/EditProfile';
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Toaster
+        position="top right"
+        toastOptions={{
+          style: {
+            fontSize: '1.8rem',
+          },
+        }}
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+      </Routes>
+    </>
   );
 }
 
