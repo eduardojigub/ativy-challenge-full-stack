9f2a9c393444b417073630a418d4760ad4cd3d9f
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 13:50:44 -0300

    feat: adding updateUser method on controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import User from '../models/userModel.js';
 export const getUserInfo = async (req, res, next) => {
   try {
     const data = await User.findById(req.user.id)
-      .select('name email address tasks');
+      .select('name email address');
     res.status(200).json(data);
   } catch (err) {
     next(err);
@@ -15,6 +15,25 @@ export const getUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.user.id).select('name email');
     res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      {
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+      },
+      {
+        new: true,
+      },
+    ).select('name email address');
+    res.status(200).json(updatedUser);
   } catch (err) {
     next(err);
   }
--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import { getUserInfo } from '../controllers/userController.js';
+import { getUserInfo, updateUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.get('/me/info/', getUserInfo);
+router.get('/me/', getUserInfo);
+router.put('/me', updateUser);
 
 export default router;
