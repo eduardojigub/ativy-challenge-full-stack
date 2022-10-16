877c2b6fe56e3f13aa4ca7e87a3031686e930f99
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 12:21:36 -0300

    feat: adding middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
 import express from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+// middlewares;
+app.use(cors()); // ajuda na conexão com o front-end, permite requests do React
+app.use(morgan('tiny')); // pacote que auxilia no controle de logs no console do node
+app.use(express.json());
+app.use(cookieParser()); // parses the incoming cookies from request to JSON value.
+
+// função que connectDB é a função que conecta o mongo ao Atlas, usando o .env
 const connectionParams = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-// função que conecta o mongo ao Atlas, usando o .env
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_CONNECTION_STRING, connectionParams);
@@ -17,9 +30,6 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
-const PORT = process.env.PORT || 8000;
-const app = express();
 
 app.listen(PORT, () => {
   connectDB();
