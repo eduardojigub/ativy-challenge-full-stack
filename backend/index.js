01e93ec80874eece2ebf11db7dd59472b5ced44f
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 12:00:43 -0300

    feat: adding .env file

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+import express from 'express';
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
