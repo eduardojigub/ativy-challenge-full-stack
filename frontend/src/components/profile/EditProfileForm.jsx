d47a671db60081d5328222c13b71d405e1209b79
eduardojigub <eduardobrunoit@gmail.com>
2022-10-16 21:59:21 -0300

    feat: changing favicon, adding styles for ProfileForm

---
+++ frontend/src/components/profile/EditProfileForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import classes from './EditProfileForm.module.scss';
+
+function EditProfileForm() {
+  return (
+    <div>EditProfileForm</div>
+  );
+}
+
+export default EditProfileForm;
